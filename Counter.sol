f9cd27f09a98152d1e9cfeb6d1108c174beb51a8
andreapn <fifi.hate.lonely@gmail.com>
2022-12-30 14:49:11 +0700

    update set get

--- Counter.sol
+++ Counter.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.17;
 
 contract Counter {
-    uint public count;
+    uint count;
+    address myAddress;
+    string d = "andreapn";
 
     // Function to get the current count
     function get() public view returns (uint) {
@@ -12,6 +14,22 @@ contract Counter {
     // Function to increment count by 1
     function inc() public {
         count += 1;
+    }
+
+    function setSender() public {
+        myAddress = msg.sender;
+    }
+
+    function getSender() public view returns (address) {
+        return myAddress;
+    }
+
+    function getD() public view returns (string memory) {
+        return d;
+    }
+
+    function setD(string memory chuoi) public {
+        d = chuoi;
     }
 
     // Function to decrement count by 1
